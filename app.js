6758e1dc2d61c84f1044a5ec55086b9fdee28ff7
Haami hosseinzadeh <tehran.motors.tm@gmail.com>
2024-12-30 17:40:53 +0330

    bug fixes

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 document.addEventListener('DOMContentLoaded', () => {
     const updatesContainer = document.getElementById('updates-container');
+    
+    // List of markdown files
+    const updates = [
+        'updates/update1.md',
+        'updates/update2.md',
+        // Add more markdown files here
+    ];
 
-    fetch('load_updates.php')
-        .then(response => response.json())
-        .then(updates => {
-            updates.forEach(update => {
+    updates.forEach(update => {
+        fetch(update)
+            .then(response => response.text())
+            .then(markdown => {
                 const updateDiv = document.createElement('div');
-                fetch(update)
-                    .then(response => response.text())
-                    .then(markdown => {
-                        updateDiv.innerHTML = marked(markdown);
-                        updatesContainer.appendChild(updateDiv);
-                    });
+                updateDiv.innerHTML = marked(markdown);
+                updatesContainer.appendChild(updateDiv);
             });
-        });
+    });
 });
--- load_updates.php
+++
@@ -1,8 +0,0 @@
-<?php
-$directory = 'updates';
-$updates = array_diff(scandir($directory), array('..', '.'));
-$updates = array_map(function($file) use ($directory) {
-    return $directory . '/' . $file;
-}, $updates);
-echo json_encode($updates);
-?>
